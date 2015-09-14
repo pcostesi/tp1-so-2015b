d4e31ccce3d6c512fc1f9092779d0bf8a162565a
Braulio  Sespede <bsespede@lab119.it.itba.edu.ar>
2015-09-14 18:00:07 -0300

    Shell

--- src/atcc/ui.c
+++ src/atcc/ui.c
@@ -4,16 +4,16 @@
 #include "./include/ui.h"
 #include "../atcd/include/atcd.h"
 
-static WINDOW *map, *cmd_log, *plane_list, *cmd;
-static WINDOW *brd_map, *brd_cmd_log, *brd_plane_list, *brd_cmd;
+static WINDOW *map, *cmd_log, *plane_list, *actions;
+static WINDOW *brd_map, *brd_cmd_log, *brd_plane_list, *brd_actions;
 static int cur_line;
+static struct atc_plane planes[MAX_PLANES];
 
 void ui_test(){
 	init();
 	while(1){
 		draw();
-		cmd_scan();
-		
+		//cmd_scan();
 	}
 	dispose();
 }
@@ -21,9 +21,44 @@ void ui_test(){
 
 void cmd_scan(){
 	char scan_cmd[32];
-	wscanw(cmd, "%s", &scan_cmd);
-	print_cmd(scan_cmd);
+	char scan_plane[32];
+	wscanw(cmd, "%s:%s", &scan_cmd, &scan_plane);
+	execute_cmd(&scan_cmd, &scan_plane);	
 	wclear(cmd);
+}
+
+void execute_cmd(char *scan_cmd, char *scan_plane){
+	struct atc_plane plane;
+	int found_plane = get_plane(scan_plane, &plane);
+	if (!found_plane){
+		print_cmd("Plane doesnt exist");
+	}
+	if(strcmp(scan_cmd, "SPDUP")){
+		set(speed_up, plane);
+	}else if(strcmp(scan_cmd, "SPDDW")){
+		set(speed_down, plane);
+	}else if(strcmp(scan_cmd, "UP")){
+		set(climb, plane);
+	}else if(strcmp(scan_cmd, "DN")){
+		set(descend, plane);
+	}else if(strcmp(scan_cmd, "TR")){
+		set(turn_right, plane);
+	}else if(strcmp(scan_cmd, "TL")){
+		set(turn_left, plane);
+	}else{
+		print_cmd("Unsupported command");
+	}
+}
+
+int get_plane(char *plane_name, struct atc_plane *plane){
+	int i;
+	for (i = 0; i < planes_num; i++){
+		if (strcmp(plane_name, planes[i].id)){
+			plane = planes[i];
+			return 1;
+		}
+	}
+	return 0;
 }
 
 void init(){
@@ -31,27 +66,27 @@ void init(){
 	init_brd();
 
 	map = newwin(MAP_HEIGHT, MAP_WIDTH, 1, 1);
-	plane_list = newwin(MAP_HEIGHT, LIST_WIDTH, 1, MAP_WIDTH+BORDER+1);
-	cmd_log = newwin(LOG_HEIGHT, LIST_WIDTH+MAP_WIDTH+BORDER, MAP_HEIGHT+BORDER+1, 1);
-	cmd = newwin(CMD_HEIGHT, LIST_WIDTH+MAP_WIDTH+BORDER, MAP_HEIGHT+LOG_HEIGHT+BORDER*2+1, 1);
+	plane_list = newwin(MAP_HEIGHT, LIST_WIDTH, 1, MAP_WIDTH+BORDER*2+1);
+	cmd_log = newwin(LOG_HEIGHT, MAP_WIDTH, MAP_HEIGHT+BORDER*2+1, 1);
+	actions = newwin(LOG_HEIGHT, LIST_WIDTH, MAP_HEIGHT+BORDER*2+1, MAP_WIDTH+BORDER*2+1);
 	cur_line = 0;	
 }
 
 void init_brd(){
-	brd_map = newwin(MAP_HEIGHT+BORDER, MAP_WIDTH+BORDER, 0, 0);
-	brd_plane_list = newwin(MAP_HEIGHT+BORDER, LIST_WIDTH+BORDER, 0, MAP_WIDTH+BORDER);
-	brd_cmd_log = newwin(LOG_HEIGHT+BORDER, LIST_WIDTH+MAP_WIDTH+BORDER*2, MAP_HEIGHT+BORDER, 0);
-	brd_cmd = newwin(1+BORDER, LIST_WIDTH+MAP_WIDTH+BORDER*2, MAP_HEIGHT+LOG_HEIGHT+BORDER*2, 0);
+	brd_map = newwin(MAP_HEIGHT, MAP_WIDTH, 0, 0);
+	brd_plane_list = newwin(MAP_HEIGHT, LIST_WIDTH, 0, MAP_WIDTH+BORDER*2);
+	brd_cmd_log = newwin(LOG_HEIGHT, MAP_WIDTH, MAP_HEIGHT+BORDER*2, 0);
+	brd_actions = newwin(LOG_HEIGHT, LIST_WIDTH, MAP_HEIGHT+BORDER*2, MAP_WIDTH+BORDER*2);
 
 	box(brd_map, 0, 0);
 	box(brd_cmd_log, 0, 0);
 	box(brd_plane_list, 0, 0);
-	box(brd_cmd, 0, 0);
+	box(brd_actions, 0, 0);
 
 	wrefresh(brd_map);
 	wrefresh(brd_cmd_log);
 	wrefresh(brd_plane_list);
-	wrefresh(brd_cmd);
+	wrefresh(brd_actions);
 }
 
 void draw(){
@@ -59,7 +94,7 @@ void draw(){
 	wclear(plane_list);	
 
 	// //!!!CHANGE TO ACTUAL FUNCTION!!!
-	struct atc_plane planes[MAX_PLANES] = {{"AA1234", 1, 2, 3}, {"XX1234", 5, 3, 1,}};
+	planes = {{"AA1234", 1, 2, 3}, {"XX1234", 5, 3, 1,}};
 	int planes_num = 2;
 	planes[0].status = landed;
 	planes[1].status = crashed;
