0b4a044d020c23a616fbc505dbbee8a13b709b37
bsespede <braulio.sespede@gmail.com>
2015-09-15 18:17:54 -0300

    Added command menu with highlights

--- src/atcc/include/ui.h
+++ src/atcc/include/ui.h
@@ -4,11 +4,13 @@
 #define MAX_PLANES 16
 #define MAX_AIRPORTS 10
 
-#define MAP_WIDTH 50
-#define MAP_HEIGHT 25
+#define MAP_WIDTH (LIST_CMDS+LIST_HEIGHT+BORDER)*2
+#define MAP_HEIGHT LIST_CMDS+LIST_HEIGHT+BORDER
+#define LIST_HEIGHT 20
 #define LIST_WIDTH 20
+#define LIST_CMDS 7
 #define LOG_HEIGHT 1
-#define CMD_HEIGHT 1
+#define CMD_HEIGHT 7
 
 #define BORDER 2
 
@@ -16,12 +18,15 @@
 
 #define TIMEOUT 250
 
+#define CMDS_NUM 6
+
 void init();
 void draw();
 void dispose();
 void clear_log();
 void ui_test();	
 void interpret_ch(int ch);
+char *get_cmdname(int cmd);
 
 #endif
 
--- src/atcc/ui.c
+++ src/atcc/ui.c
@@ -5,8 +5,8 @@
 #include "./include/ui.h"
 #include "../atcd/include/atcd.h"
 
-static WINDOW *map, *cmd_log, *plane_list;
-static WINDOW *brd_map, *brd_cmd_log, *brd_plane_list;
+static WINDOW *map, *cmd_log, *plane_list, *cmds;
+static WINDOW *brd_map, *brd_cmd_log, *brd_plane_list, *brd_cmds;
 static int cur_line;
 
 static struct atc_plane planes[MAX_PLANES] = {{"AA1234", 1, 2, 3}, {"XX1234", 5, 3, 1,}};
@@ -30,16 +30,16 @@ void interpret_ch(int ch){
 		if (cur_plane == -1){
 			if (ch-'0' < planes_num){
 				cur_plane = ch-'0';
-				struct atc_plane plane = planes[cur_page*PLANES_PER_PAGE+cur_plane];
-				clear_log();
-				mvwprintw(cmd_log, 0, 0, "%s %s", "Selected plane",plane.id);
+				//clear_log();
+				//struct atc_plane plane = planes[cur_page*PLANES_PER_PAGE+cur_plane];
+				//mvwprintw(cmd_log, 0, 0, "%s %s", "Selected plane",plane.id);
 			}
 			return;
 		}else if (cur_cmd == -1){
 			if (ch >= '0' && ch <= '6'){
 				cur_cmd = ch-'0';
-				clear_log();
-				mvwprintw(cmd_log, 0, 0, "%s %d", "cur cmd is ",cur_cmd);
+				//clear_log();
+				//mvwprintw(cmd_log, 0, 0, "%s %s", "Select command", get_cmdname(cur_cmd));
 			}
 			return;
 		}
@@ -80,26 +80,29 @@ void interpret_ch(int ch){
 
 void init(){
 	initscr();
-	//cbreak();
 	noecho();
 	keypad(cmd_log, TRUE);
 	nodelay(cmd_log, FALSE);
 
 	brd_map = newwin(MAP_HEIGHT+BORDER, MAP_WIDTH+BORDER, 0, 0);
-	brd_plane_list = newwin(MAP_HEIGHT+BORDER, LIST_WIDTH+BORDER, 0, MAP_WIDTH+BORDER);
+	brd_plane_list = newwin(LIST_HEIGHT+BORDER, LIST_WIDTH+BORDER, 0, MAP_WIDTH+BORDER);
+	brd_cmds = newwin(CMD_HEIGHT+BORDER, LIST_WIDTH+BORDER, LIST_HEIGHT+BORDER, MAP_WIDTH+BORDER);
 	brd_cmd_log = newwin(LOG_HEIGHT+BORDER, MAP_WIDTH+LIST_WIDTH+BORDER*2, MAP_HEIGHT+BORDER, 0);
 
 	map = newwin(MAP_HEIGHT, MAP_WIDTH, 1, 1);
-	plane_list = newwin(MAP_HEIGHT, LIST_WIDTH, 1, MAP_WIDTH+BORDER+1);
+	plane_list = newwin(LIST_HEIGHT, LIST_WIDTH, 1, MAP_WIDTH+BORDER+1);
+	cmds = newwin(CMD_HEIGHT, LIST_WIDTH, LIST_HEIGHT+BORDER+1, MAP_WIDTH+BORDER+1);
 	cmd_log = newwin(LOG_HEIGHT, MAP_WIDTH+LIST_WIDTH+BORDER, MAP_HEIGHT+BORDER+1, 1);
 	
-		box(brd_map, 0, 0);
+	box(brd_map, 0, 0);
 	box(brd_cmd_log, 0, 0);
 	box(brd_plane_list, 0, 0);
+	box(brd_cmds, 0, 0);
 
 	wrefresh(brd_map);
 	wrefresh(brd_cmd_log);
 	wrefresh(brd_plane_list);
+	wrefresh(brd_cmds);
 
 	cur_line = 0;	
 	cur_page = 0;
@@ -110,6 +113,7 @@ void init(){
 void draw(){
 	wclear(map);
 	wclear(plane_list);	
+	wclear(cmds);	
 
 	// //!!!CHANGE TO ACTUAL FUNCTION!!!
 	planes_num = 2;
@@ -138,7 +142,7 @@ void draw(){
 	for (i = 0; i < planes_num; i++){
 		mvwprintw(map, planes[i].y, planes[i].x, "+");
 		if (i >= cur_page*PLANES_PER_PAGE && i < PLANES_PER_PAGE*(cur_page+1)){
-			mvwprintw(plane_list, i*2, 0, "%s (%d, %d, %d)", planes[i].id, planes[i].x, planes[i].y, planes[i].z);
+			mvwprintw(plane_list, i*2, 0, "%d: %s (%d, %d, %d)", i%PLANES_PER_PAGE, planes[i].id, planes[i].x, planes[i].y, planes[i].z);
 			mvwchgat(plane_list, i*2, 0, -1, A_BOLD, 0, NULL);
 			if (cur_plane >= 0 && i == cur_page*PLANES_PER_PAGE+cur_plane){
 				mvwchgat(plane_list, i*2, 0, -1, A_REVERSE, 0, NULL);
@@ -157,7 +161,15 @@ void draw(){
 		}		
 	}
 
+	for (i = 0; i < CMDS_NUM; i++){
+		mvwprintw(cmds, i, 0, "%d: %s", i, get_cmdname(i));
+		if (cur_cmd != -1){
+			mvwchgat(cmds, cur_cmd, 0, -1, A_REVERSE, 0, NULL);
+		}
+	}
+
 	wrefresh(map);
+	wrefresh(cmds);
 	wrefresh(plane_list);
 	wrefresh(cmd_log);
 }
@@ -165,6 +177,23 @@ void draw(){
 void clear_log(){
 	wmove(cmd_log, 0, 0);
 	wclrtoeol(cmd_log);
+}
+
+char *get_cmdname(int cmd){
+	switch (cmd){
+		case 0:
+			return "Speed up";
+		case 1:	
+			return "Speed down";
+		case 2:
+			return "Ascend";
+		case 3:
+			return "Descend";
+		case 4:
+			return "Turn right";
+		default:
+			return "Turn left";
+	}
 }
 
 void dispose(){
