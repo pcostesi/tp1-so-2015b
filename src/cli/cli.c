fec08c0cfe7ef9a5de54605b83a0342610cdaf9b
sebamaio <sebamaio@gmail.com>
2015-09-24 14:02:47 -0300

    client prototypes

--- src/cli/cli.c
+++ src/cli/cli.c
@@ -1,4 +1,27 @@
+#include "cli.h"
 #include "atcd.h"
+
+int get_planes(struct atc_plane buff[])
+{
+
+}
+
+int get_airports()
+{
+
+}
+
+void create_plane(void)
+{
+
+}
+
+int set(enum atc_command)
+{
+
+}
+
+
 
 int atcd_test(void) {
     return -1;
---
+++ src/cli/include/cli.h
@@ -0,0 +1,11 @@
+#ifndef __ATCD_H
+#define __ATCD_H 1
+
+
+
+/*Function Headers*/
+
+
+
+
+#endif
