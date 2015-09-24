2d5b8adf48fab86bb890b06b91b2a9ac57df4910
bsespede <braulio.sespede@gmail.com>
2015-09-24 20:33:18 -0300

    srv h

---
+++ src/srv/include/srv.h
@@ -0,0 +1,23 @@
+#ifndef __SRV_H
+#define __SRV_H 1
+
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+
+struct list{
+	listener_node head;
+	listener_node tail;
+};
+
+struct listener_node{
+	pid_t pid;
+	listener_node next;
+};
+
+void listen_client();
+void listen_channel();
+void fork_client();
+void kill_client(pid_t pid);
+
+
