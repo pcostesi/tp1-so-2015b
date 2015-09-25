9658d41786a39695fb65844611e770856d1786c6
Pablo Alejandro Costesich <rhfixer@gmail.com>
2015-09-25 18:46:01 -0300

    Add more serializations

--- src/protocol/include/serialize.h
+++ src/protocol/include/serialize.h
@@ -4,21 +4,28 @@
 #include "atcd.h"
 
 union _atcp_msg {
-	struct atc_plane plane;
-	struct atc_airport airport;
-	struct {
+    struct atc_plane plane;
+    struct atc_airport airport;
+    struct {
         enum atc_req_type type;
         char id[ATCD_ID_LENGTH];
-	} req;
+        struct atc_plane plane;
+    } req;
 };
 
 #define ATCP_MSG_LEN (sizeof(union _atcp_msg))
 
-void atc_plane_to_wire(struct atc_plane * plane, unsigned char wire[ATCP_MSG_LEN]);
-void atc_wire_to_plane(struct atc_plane * plane, unsigned char wire[ATCP_MSG_LEN]);
+unsigned char * atc_plane_to_wire(struct atc_plane * plane, unsigned char wire[ATCP_MSG_LEN]);
+unsigned char * atc_wire_to_plane(struct atc_plane * plane, unsigned char wire[ATCP_MSG_LEN]);
 
-void atc_airport_to_wire(struct atc_airport * airport, unsigned char wire[ATCP_MSG_LEN]);
-void atc_wire_to_airport(struct atc_airport * airport, unsigned char wire[ATCP_MSG_LEN]);
+unsigned char * atc_airport_to_wire(struct atc_airport * airport, unsigned char wire[ATCP_MSG_LEN]);
+unsigned char * atc_wire_to_airport(struct atc_airport * airport, unsigned char wire[ATCP_MSG_LEN]);
+
+
+unsigned char * atc_req_to_wire(struct atc_req * req, unsigned char wire[ATCP_MSG_LEN]);
+unsigned char * atc_wire_to_req(struct atc_req * req, unsigned char wire[ATCP_MSG_LEN]);
+
+unsigned char * atc_res_to_wire(struct atc_res * res, unsigned char wire[ATCP_MSG_LEN]);
+unsigned char * atc_wire_to_res(struct atc_res * res, unsigned char wire[ATCP_MSG_LEN]);
 
 #endif
-
--- src/protocol/proto.c
+++ src/protocol/proto.c
@@ -6,30 +6,41 @@
 
 int atc_connect(struct atc_conn * conn)
 {
-	return 0;
+    return 0;
 }
 
 
 int atc_listen(struct atc_conn * conn)
 {
-	return 0;
+    return 0;
 }
 
 
 int atc_close(struct atc_conn * conn)
 {
-	return 0;
+    return 0;
 }
 
 
 int atc_request(struct atc_conn * conn)
 {
-	return 0;
+    unsigned char send_buffer[ATCP_MSG_LEN];
+    unsigned char recv_buffer[ATCP_MSG_LEN];
+    int response;
+
+    atc_req_to_wire(&conn->req, send_buffer);
+    response = transport_send(&conn->addr, send_buffer, sizeof(send_buffer));
+    if (response == -1) {
+        return -1;
+    }
+    response = transport_recv(&conn->addr, recv_buffer, sizeof(recv_buffer));
+    atc_wire_to_res(&conn->res, recv_buffer);
+    return 0;
 }
 
 
 int atc_reply(struct atc_conn * conn, atc_reply_handler handler)
 {
-	return 0;
+    return 0;
 }
 
